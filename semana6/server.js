570efdb3a1cb6c1158c84c19939dece264b6b835
Jun0verlord <juanmunos509@gmail.com>
2026-06-12 22:20:08 +0000

    server y prueba hechos

--- semana6/server.js
+++ semana6/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Servidor de semana 6');
+app.use(express.json());
+
+app.post('/registro', (req, res) => {
+
+  const nombre = req.body.nombre;
+  const mensaje = req.body.mensaje;
+
+  res.json({
+    estado: "Datos recibidos",
+    nombre: nombre,
+    mensaje: mensaje
+  });
+
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Servidor escuchando en ${port}`));
+app.post('/incidencia', (req, res) => {
+
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
